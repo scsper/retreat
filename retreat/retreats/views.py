1d05dbaa979a4d28f86d7281cedb86130a9beb26
Scott Sperling <ssperlin@yahoo-inc.com>
2013-12-26 01:01:39 -0800

    updating view to use common idiom

--- retreat/retreats/views.py
+++ retreat/retreats/views.py
@@ -1,12 +1,7 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
-
+from django.shortcuts import render
 from retreats.models import Retreat
 
 def index(request):
     retreat_list = Retreat.objects.all()
-    template = loader.get_template('retreats/index.html')
-    context = RequestContext(request, {
-        'retreat_list': retreat_list,
-    })
-    return HttpResponse(template.render(context))
+    context = { 'retreat_list': retreat_list }
+    return render(request, 'retreats/index.html', context)
